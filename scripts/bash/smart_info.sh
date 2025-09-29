414c79f3fe8d831e74cb5c01e0656a4458889898
Aboubakr2000Cloud <aboubacre.ijannane@gmail.com>
2025-09-29 22:06:35 +0100

    Week 1: Linux fundamentals - working bash script with config management

--- scripts/bash/smart_info.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source configs/system.conf
-echo "$GREETING"
-if [ "$SHOW_DATE" = "yes" ]; then echo "current date: $(date)"
-fi
-if [ "$SHOW_USER" = "yes" ]; then echo "current user: $(whoami)"
-fi
-if [ "$SHOW_DISK" = "yes" ]; then echo "disk space: $(df -h /)"
-fi 
--- scripts/bash/system_info.sh
+++ scripts/bash/system_info.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-echo "system  info!"
-whoami
-date
-pwd
+source configs/system.conf
+echo "$GREETING"
+if [ "$SHOW_DATE" = "yes" ]; then echo "current date: $(date)"
+fi
+if [ "$SHOW_USER" = "yes" ]; then echo "current user: $(whoami)"
+fi
+if [ "$SHOW_DISK" = "yes" ]; then echo "disk space: $(df -h /)"
+fi 
+
